2c14941d7f91701733eae2bb74fdb7ceae92f5bf
Rahul Kumar <152674805+rk80532@users.noreply.github.com>
2026-03-22 20:41:24 +0530

    Delete chess-ai-main directory

--- chess-ai-main/Backend/demo/src/main/java/com/example/demo/enums/GameResultType.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.enums;
-
-public enum GameResultType {
-    CHECKMATE,
-    DRAW,
-    RESIGN,
-    TIMEOUT
-}
-
--- chess-ai-main/Backend/demo/src/main/java/com/example/demo/enums/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.enums;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
-
